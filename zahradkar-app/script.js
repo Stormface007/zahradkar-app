23b9841f9e255a098cfb006612b4abe9547c1d79
Stormface007 <pajaprochazka@seznam.cz>
2025-06-17 22:41:32 +0200

    Update script.js

--- zahradkar-app/script.js
+++ zahradkar-app/script.js
@@ -54,8 +54,7 @@ function loadZahony() {
           <td>${zahon.NazevZahonu}</td>
           <td>${zahon.Velikost_m2} m²</td>
          <td><button onclick="deleteZahon(${zahon.ZahonID})" class="deleteBtn" title="Smazat">
-  🗑️
-</button></td>
+          🗑️</button></td>
         tbody.appendChild(tr);
       });
     });
