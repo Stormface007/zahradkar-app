9599b155237dcc825965750103e5cf8890aa2025
Stormface007 <pajaprochazka@seznam.cz>
2025-06-19 14:56:58 +0200

    Update proxy.js

--- zahradkar-app/netlify/functions/proxy.js
+++ zahradkar-app/netlify/functions/proxy.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 // Nahraď svou funkční Google Apps Script URL
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyKIQbb0yKfrw5KJ4m8SQeo41eLyRyIU1qUG7sv5VGTkmrjPIaC7PyrrplsU7RMezVa/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_hSkdrP0vjkcUOlWfAEKw2mkpMQ7kWqP6btQhO84TBRCY1jZS1IrD8t510cIiTvj8/exec";
 
 exports.handler = async function(event, context) {
     const params = event.rawQuery ? "?" + event.rawQuery : "";
