98e41903f99d95eb13c71339ea9f3357b2185d3f
Stormface007 <pajaprochazka@seznam.cz>
2025-06-19 15:01:16 +0200

    Update script.js

--- zahradkar-app/script.js
+++ zahradkar-app/script.js
@@ -1,7 +1,7 @@
 // - Dynamické <select> pro plodiny a hnojiva
 // - Výpis a mazání událostí pro konkrétní záhon
 
-const SERVER_URL = 'https://script.google.com/macros/s/AKfycby_hSkdrP0vjkcUOlWfAEKw2mkpMQ7kWqP6btQhO84TBRCY1jZS1IrD8t510cIiTvj8/exec';
+const SERVER_URL = 'https://script.google.com/macros/s/AKfycbxQzLhh7Tf97qutgcIL_jXFZID95dMp8NQLkQgJo3oenxtJPui3L-45WQz_Ysg_SmgX/exec';
 
 let aktualniZahon = null;
 
