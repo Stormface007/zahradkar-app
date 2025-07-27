4223adc9a9d810e8fac3e2f7146daa2b2dc24228
Stormface007 <pajaprochazka@seznam.cz>
2025-07-27 14:19:45 +0200

    Update script.js

--- zahradkar-app/script.js
+++ zahradkar-app/script.js
@@ -1,5 +1,5 @@
 // — Vaše URL GAS webappu —  
-const SERVER_URL = "https://script.google.com/macros/s/AKfycbwDRq4THf_PLrdNF7Rzia8sT71CSGPQoRJWbU0OiGoscHrBormUAN6uFDVxRLwNpTFv/exec";
+const SERVER_URL = "https://script.google.com/macros/s/AKfycbxsfCoXIJ1YrP-S4gzIEyK60KuTwesmeyTkApexgPYfgyYojGljEBNLjhJbSxLFRg/exec";
 
 let aktualniZahon = null;
 
