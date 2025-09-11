e4a058e8061ae772c73f8bb183ec41ba6b224266
Stormface007 <pajaprochazka@seznam.cz>
2025-09-11 21:25:34 +0200

    Update proxy.js

--- zahradkar-app/netlify/functions/proxy.js
+++ zahradkar-app/netlify/functions/proxy.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 
 // Základní adresa vašeho GAS webappu
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxnWjRwzF895agX_PK0snCfNOp7h6RSUyIWEw0Ya6rGC7mCPmBcN5RSK_oEp6PUJLg9/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxqNqvm7OXgo2E5HYbusxM4ZBn10zwV5ZkfTEnB4zepGI8w_DdLZanDgLyWexwvAI71/exec";
 
 // Pokud není GAS_URL v env, použijeme tuhle konstantu
 const GAS_URL = process.env.GAS_URL || APPS_SCRIPT_URL;
