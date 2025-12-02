ff94617cec314c69e7bf43f1ca2d05ac2bcf9f3a
Stormface007 <pajaprochazka@seznam.cz>
2025-12-02 16:29:36 +0100

    Fix comment syntax for markdown to HTML conversion

--- zahradkar-app/script.js
+++ zahradkar-app/script.js
@@ -1097,7 +1097,7 @@ obsahEl.innerHTML = "<p>Pro tuto kombinaci zatím není k dispozici doporučení
 return;
 }
 
-/ Převod markdown na HTML
+// Převod markdown na HTML
 let html = doporuceni
 .replace(/**(.*?)**/g, "<strong>$1</strong>") // tučné
 .replace(/\n/g, "
