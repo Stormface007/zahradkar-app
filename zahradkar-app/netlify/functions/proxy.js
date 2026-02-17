88c412048e54b24f706074282a8be07800f4b8fc
Stormface007 <pajaprochazka@seznam.cz>
2026-02-17 14:18:37 +0100

    Update Google Apps Script URL in proxy.js

--- zahradkar-app/netlify/functions/proxy.js
+++ zahradkar-app/netlify/functions/proxy.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 
 // Základní adresa vašeho GAS webappu
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzafCI57cyr3iv2PfCbEzKBe22IkcY__7Mbm4UT-ZlspcswAP1aq0JJk23qoUSUtQnQ/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz3L5_RbYezImCJeqqkTCmJTrfBfLtLurgXAO2VD2MD-IgUYLOZeQF-VZOjNN7C7PM_/exec";
 
 // Pokud není GAS_URL v env, použijeme tuhle konstantu
 const GAS_URL = process.env.GAS_URL || APPS_SCRIPT_URL;
